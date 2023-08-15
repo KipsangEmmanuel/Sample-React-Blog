1b20cc543ee10cc4f440eae88f9ee549043681a8
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-08-15 09:55:51 -0700

    useStae hook

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
- import React from 'react'
- 
+ import React, { useState } from 'react'
+
  const Home = () => {
+    // let name = "Kipsang"
+    const [name, setName] = useState("Emmanuel")
+    const [age, setAge] = useState(25);
+    const handleClick = () => {
+        setName("Kipsang");
+        setAge(30);
+    }
    return (
      <div className='home'>
         <h2>Home Page</h2>
+        <p>{ name } is { age } years old</p>
+        <button onClick={handleClick}>Click me</button>
      </div>
    )
  }
