91dc6eb18aeec49817982841240875788b967e87
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-08-15 09:27:16 -0700

    initialize the blog

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from './Home';
+import NavBar from './NavBar';
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar />
+      <div className='content'>
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
