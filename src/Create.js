d4d7c36ad091b290bd6fac734d0a9f20ebc5fe40
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-08-17 07:33:30 -0700

    make a POST request

--- src/Create.js
+++ src/Create.js
@@ -3,11 +3,30 @@ import React, { useState } from 'react'
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState()
+    const [author, setAuthor] = useState();
+    const [isPending, setIsPending] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        })
+        .then(() => {
+            console.log('New blog added');
+            setIsPending(false)
+        })
+
+    }
 
   return (
     <div className='create'>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Blog title</label>
             <input 
                 type="text"
@@ -30,13 +49,11 @@ const Create = () => {
                 <option value="mario">Mario</option>
                 <option value="yoshi">Yoshi</option>
             </select>
-            <button>Add Blog</button>
+            {!isPending && <button>Add Blog</button>}
+            {isPending && <button disabled>Adding blog...</button>}
             <p>{title}</p>
             <p>{body}</p>
-            <p>{author}</p>
-
-
-     
+            <p>{author}</p>    
         </form>
     </div>
   )
