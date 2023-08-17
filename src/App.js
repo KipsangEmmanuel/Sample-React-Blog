f544808de1f3cfc83f34ad8cf7be33e9874b0d1d
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-08-16 21:38:41 -0700

    Route Parameters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from './Home';
 import NavBar from './NavBar';
 import {Routes, Route, BrowserRouter, } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
@@ -11,7 +13,11 @@ function App() {
         <div className='content'>
           {/* <Home /> */}
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+
+
 
           </Routes>
         </div>
